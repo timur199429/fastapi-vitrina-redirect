877d760bff7b1b8dff58547c2aeb09e0226cd23f
timur199429 <timur199429@gmail.com>
2025-03-06 17:41:32 +0300

    replace pg8000 to psycopg2

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv; load_dotenv()
 
 # Create the database engine
-URL_DATABASE = f'postgresql+pg8000://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}'
+URL_DATABASE = f'postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:5432/{os.getenv('DB_NAME')}'
 engine = create_engine(URL_DATABASE)
 
 def db_init():

69b413506a7e0376530caaf100e50c67c0443884
timur199429 <timur199429@gmail.com>
2025-03-06 17:52:48 +0300

    change URL_DATABASE

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv; load_dotenv()
 
 # Create the database engine
 URL_DATABASE = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
 engine = create_engine(URL_DATABASE)
 
 def db_init():
